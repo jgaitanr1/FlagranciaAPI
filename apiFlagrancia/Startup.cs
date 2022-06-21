8568d4a45a7a41407159b1cd98d3d0caf9a42a5e
jgaitanr <jgaitanr@outlook.com>
2022-06-21 14:33:17 -0500

    rumeplz

--- apiFlagrancia/Startup.cs
+++ apiFlagrancia/Startup.cs
@@ -55,12 +55,12 @@ namespace apiFlagrancia
                 //app.UseSwagger();
                 //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "apiFlagrancia v1"));
             }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+            //else
+            //{
+            //    app.UseExceptionHandler("/Error");
+            //    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            //    app.UseHsts();
+            //}
 
             app.UseHttpsRedirection();
 
